ddee974a42c71cd66e104b5d505e1abf0d6b1b39
Ariful Haque Mohsin <arifbsns22@gmail.com>
2025-01-02 00:35:45 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,91 @@
-import 'package:flutter/material.dart';
+//------------Answer of question no. A ----------------------------
+abstract class Role {
+  void displayRole();
+}
+//------------Answer of question no. B ----------------------------
+class Person implements Role {
+  String name;
+  int age;
+  String address;
+
+  Person(this.name, this.age, this.address);
+
+  @override
+  void displayRole() {}
+
+  String get getName => name;
+  int get getAge => age;
+  String get getAddress => address;
+}
+//------------Answer of question no. C ----------------------------
+class Student extends Person {
+  String studentID;
+  String grade;
+  List<double> courseScores;
+
+  Student(String name, int age, String address, this.studentID, this.grade,
+      this.courseScores)
+      : super(name, age, address);
+
+  @override
+  void displayRole() {
+    print("Role: Student");
+  }
+
+  double calculateAverageScore() {
+    return courseScores.reduce((a, b) => a + b) / courseScores.length;
+  }
+}
+//------------Answer of question no. D ----------------------------
+class Teacher extends Person {
+  String teacherID;
+  List<String> coursesTaught;
+
+  Teacher(
+      String name, int age, String address, this.teacherID, this.coursesTaught)
+      : super(name, age, address);
+
+  @override
+  void displayRole() {
+    print("Role: Teacher");
+  }
+
+  void displayCoursesTaught() {
+    print("Courses Taught:");
+    for (var course in coursesTaught) {
+      print("- $course");
+    }
+  }
+}
+//------------Answer of question no. E ----------------------------
+class StudentManagementSystem {
+  void main() {
+    // Create a Student instance
+    Student student =
+        Student("John Doe", 20, "123 Main St", "S123", "A", [90.0, 85.0, 82.0]);
+
+    // Create a Teacher instance
+    Teacher teacher = Teacher(
+        "Mrs. Smith", 35, "456 Oak St", "T456", ["Math", "English", "Bangla"]);
+
+    // Display Student information
+    print("Student Information:\n");
+    student.displayRole();
+    print("Name: ${student.getName}");
+    print("Age: ${student.getAge}");
+    print("Address: ${student.getAddress}");
+    print("Average Score: ${student.calculateAverageScore()}\n");
+
+    // Display Teacher information
+    print("Teacher Information:\n");
+    teacher.displayRole();
+    print("Name: ${teacher.getName}");
+    print("Age: ${teacher.getAge}");
+    print("Address: ${teacher.getAddress}");
+    teacher.displayCoursesTaught();
+  }
+}
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  StudentManagementSystem().main();
 }
